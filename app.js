dc3675f0152ab4a043fe275324d00fffb80f2860
Ryan Lee <ryanm.lee11@gmail.com>
2020-10-14 22:49:58 -0400

    Initial scrolling pixel counter for vertical scroll

---
+++ app.js
@@ -0,0 +1,7 @@
+const translate = document.querySelectorAll(".translate");
+
+window.addEventListener('scroll', () => {
+    //Get number of pixels we scroll upward and place in variable "scroll"
+    let scroll = window.pageYOffset;
+    console.log(scroll);
+})

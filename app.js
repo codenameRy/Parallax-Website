50ecfebf8ca579dd2a476ded05bc8aed8e6c63fc
Ryan Lee <ryanm.lee11@gmail.com>
2020-10-14 23:16:26 -0400

    Update

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ const translate = document.querySelectorAll(".translate");
 window.addEventListener('scroll', () => {
     //Get number of pixels we scroll upward and place in variable "scroll"
     let scroll = window.pageYOffset;
-    console.log(scroll);
+    // console.log(scroll);
+
+    //For each element, get the value of attribute "data-speed"
+    translate.forEach(element => {
+        let speed = element.dataset.speed;
+        // console.log(speed);
+        //Move each elemnet based on the result of mutiplying scroll value by speed value
+        element.style.transform = `translateY(${scroll * speed}px)`;
+    })
 })

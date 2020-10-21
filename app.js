a30f6612ffb41b21a02a2dc3d8a64e4aa0a4d4e2
Ryan Lee <ryanm.lee11@gmail.com>
2020-10-20 23:17:11 -0400

    Update

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const translate = document.querySelectorAll(".translate");
 //Make title disappear as we scroll the page
 const big_title = document.querySelector(".big_title");
 const header = document.querySelector("header");
-
+const shadow = document.querySelector(".shadow");
+const content = document.querySelector(".content");
+const section = document.querySelector("section");
 //Get height of header
 let header_height = header.offsetHeight;
+let section_height = section.offsetHeight;
 
-console.log(header_height);
+// console.log(section_height);
 
 window.addEventListener('scroll', () => {
     //Get number of pixels we scroll upward and place in variable "scroll"
     let scroll = window.pageYOffset;
     // console.log(scroll);
+    //As we scroll page, get the top position of the section relative to the viewport
+    let sectionY = section.getBoundingClientRect();
+    console.log(sectionY.top);
+
 
     //For each element, get the value of attribute "data-speed"
     translate.forEach(element => {
@@ -22,5 +29,7 @@ window.addEventListener('scroll', () => {
         element.style.transform = `translateY(${scroll * speed}px)`;
     })
     //Set the opacity value of the title to the result of the mathematic formula value between 0 and 1 based on the scroll value
-    big_title.style.opacity = - scroll / (header_height / 2) + 1
+    big_title.style.opacity = - scroll / (header_height / 2) + 1;
+    //Set th height of the shador to the result of the mathematic formula
+    shadow.style.height = `${scroll * 0.5 + 300}px`;
 })
